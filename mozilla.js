303dfee045b67fd950e5bf2928781043c6a7bbcd
Sakoudamsak <hangoudamsak@gmail.com>
2025-05-18 12:24:17 +0700

    Add files via upload

---
+++ mozilla.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.sidebar a').forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const id = link.getAttribute('href').substring(1);
+      document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
